a9e75e537ba7f30f58986f42bf3ffaa0a7ba0b80
Jill-Ann <jillannkay@gmail.com>
2021-01-05 17:39:14 +0100

    Remove comments from JS

--- scripts.js
+++ scripts.js
@@ -9,25 +9,18 @@ const getFilms = () => {
     .then((response) => response.json())
     .then((data) => {
       data.forEach((movie) => {
-        // Create a div with a card class
         const card = document.createElement("div");
         card.setAttribute("class", "card");
 
-        // Create an h2 and set the text content to the film's title
         const h2 = document.createElement("h2");
         h2.textContent = movie.title;
 
-        // Create a p and set the text content to the film's description
         const p = document.createElement("p");
         movie.description = movie.description.substring(0, 400);
         p.textContent = `${movie.description}...`;
         const p2 = document.createElement("p");
-        p2.textContent = "fetch API";
-
-        // Append the cards to the container element
         container.appendChild(card);
 
-        // Each card will contain an h1 and a p
         card.appendChild(h2);
         card.appendChild(p);
         card.appendChild(p2);
